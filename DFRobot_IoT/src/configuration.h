5c709c6462e07b566d39f29a64de33e62cfa848e
Ahmad Radhy <36830633+radhyahmad@users.noreply.github.com>
2022-03-10 10:11:33 +0700

    Update configuration.h

--- DFRobot_IoT/src/configuration.h
+++ DFRobot_IoT/src/configuration.h
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 
 // Fill in  your WiFi networks SSID and password
-#define SECRET_SSID "LANTAI BAWAH 2"
-#define SECRET_PASS "ibudini17"
+#define SECRET_SSID ""
+#define SECRET_PASS ""
 
 // Fill in the hostname of your IoT broker
 #define SECRET_BROKER "broker.qubitro.com"  
@@ -12,5 +12,5 @@
 #define MQTT_USERNAME "" 
 #define MQTT_PASSWORD "" 
 
-#define DEVICE_ID "acf5d2f3-8c67-4737-b33d-c818242e5361"
-#define DEVICE_TOKEN "RI1zbk$rySvOZdqd3DEEoyo2n42n9yQxLDBYSns9"
+#define DEVICE_ID ""
+#define DEVICE_TOKEN ""
